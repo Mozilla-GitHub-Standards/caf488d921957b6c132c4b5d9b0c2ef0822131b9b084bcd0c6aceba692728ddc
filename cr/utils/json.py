8cbc68c95d02bb6f41696850f1f205dceb7bdc19
Scott Idler <scott.a.idler@gmail.com>
2018-08-09 15:52:28 -0700

    added json_print to json utils

--- cr/utils/json.py
+++ cr/utils/json.py
@@ -4,3 +4,7 @@ import json
 def jsonify(string):
     return json.loads(str(string))
 
+def print_json(d, *exclusions):
+    [d.pop(exclusion, None) for exclusion in exclusions]
+    print(json.dumps(d, indent=2, sort_keys=True))
+
