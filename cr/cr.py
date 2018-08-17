b9ef39e016d932d1378aed1aa34924a3d91bfee6
Scott Idler <scott.a.idler@gmail.com>
2018-08-17 11:13:01 -0700

    get, put, post and delete infra to ChangeRequest

--- cr/cr.py
+++ cr/cr.py
@@ -1,42 +1,105 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from cr.utils.json import print_json
-from cr.utils.asyncrequests import AsyncRequests
+import requests
+from attrdict import AttrDict
+from urllib.parse import urljoin
+from itertools import product
 
+from cr.utils.asyncrequests import AsyncRequests
+from cr.utils.json import json_print
 from cr.utils.fmt import *
 
+class JsonsDontMatchPathsError(Exception):
+    def __init__(self, jsons, paths):
+        len_jsons = len(jsons) if isinstance(jsons, list) else None
+        len_paths = len(paths) if isinstance(paths, list) else None
+        msg = fmt('len(jsons) -> {len_jsons} != len(paths) -> {len_paths}; jsons={jsons}, paths={paths}')
+        super(JsonsDontMatchPathsError, self).__init__(msg)
+
+class ChangeRequestPathError(Exception):
+    def __init__(self, path_or_paths):
+        msg = fmt('error with ChangeRequest param path(s) = {path_or_paths}')
+        super(ChangeRequestPathError, self).__init__(msg)
+
 class ChangeRequest(object):
-    def __init__(self):
-        pass
-
-    def execute(self, command, **kwargs):
-        return {
-            'create': self.create,
-            'show': self.show,
-        }[command](**kwargs)
-
-    def create(self,
-        planned_start_date=None,
-        planned_stop_date=None,
-        change_plan=None,
-        short_description=None,
-        business_impact=None,
-        change_impact=None,
-        user_impact=None,
-        security_risk_level=None,
-        change_frequency=None,
-        test_plan=None,
-        post_implementation_plan=None,
-        customer_end_user_plan=None,
-        rollback_procedure=None,
-        peer_review_date=None,
-        vendor_name=None,
-        planned_downtime=None,
+    def __init__(self,
+        baseurl=None,
+        auth=None,
+        headers=None,
+        debug=False,
         verbose=False,
         **kwargs):
 
-        print_json(locals(), 'self', 'verbose', 'kwargs')
+        self.baseurl = baseurl
+        self.auth = auth
+        self.headers = headers
+        self.debug = debug
+        self.verbose = verbose
+        self.args = AttrDict(kwargs)
+        self.ar = AsyncRequests()
 
-    def show(self, **kwargs):
-        pass
+    def keywords(self, path=None, **kw):
+        if not path:
+            raise ChangeRequestPathError(path)
+        kw['url'] = urljoin(self.baseurl, path)
+        kw['auth'] = kw.get('auth', self.auth)
+        kw['headers'] = kw.get('headers', {
+            'Content-Type': 'application/json',
+            'User-Agent': 'autocert',
+        })
+        return kw
+
+    def request(self, method, **kw):
+        return self.ar.request(method, **self.keywords(**kw))
+
+    def get(self, path=None, **kw):
+        return self.request('GET', path=path, **kw)
+
+    def put(self, path=None, **kw):
+        return self.request('PUT', path=path, **kw)
+
+    def post(self, path=None, **kw):
+        return self.request('POST', path=path, **kw)
+
+    def delete(self, path=None, **kw):
+        return self.request('DELETE', path=path, **kw)
+
+    def requests(self, method, paths=None, jsons=None, **kw):
+        if not paths or not hasattr(paths, '__iter__'):
+            raise ChangeRequestPathError(paths)
+        if jsons:
+            if len(jsons) != len(paths):
+                raise JsonsDontMatchPathsError(jsons, paths)
+            kws = [self.keywords(path=path, json=json, **kw) for (path, json) in product(paths, jsons)]
+        else:
+            kws = [self.keywords(path=path, **kw) for path in paths]
+        return self.ar.requests(method, *kws)
+
+    def gets(self, paths=None, jsons=None, **kw):
+        return self.requests('GET', paths=paths, jsons=jsons, **kw)
+
+    def puts(self, paths=None, jsons=None, **kw):
+        return self.requests('PUT', paths=paths, jsons=jsons, **kw)
+
+    def posts(self, paths=None, jsons=None, **kw):
+        return self.requests('POST', paths=paths, jsons=jsons, **kw)
+
+    def deletes(self, paths=None, jsons=None, **kw):
+        return self.requests('DELETE', paths=paths, jsons=jsons, **kw)
+
+    def execute(self):
+        return {
+            'create': self.create,
+            'show': self.show,
+        }[self.args.command]()
+
+    def create(self):
+        results = [dict(self.args)]
+        send = AttrDict(url='url', json=dict(self.args))
+        recv = AttrDict(status=200, json=dict(results=results))
+        call = AttrDict(response=None, send=send, recv=recv)
+        return call
+
+    def show(self):
+        raise NotImplementedError
